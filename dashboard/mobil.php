b3ffc4f7b8c0ea7f9d46fb8dfe5293cdf90af7d3
IchsanHanifdeal <108577976+IchsanHanifdeal@users.noreply.github.com>
2023-12-09 09:49:56 +0700

    commit

--- dashboard/mobil.php
+++ dashboard/mobil.php
@@ -1,6 +1,5 @@
-<?php
-require 'header.php';
-?>
+<?php require 'header.php'; ?>
+
 <!-- Content Wrapper -->
 <div id="content-wrapper" class="d-flex flex-column">
 
@@ -16,10 +15,11 @@ require 'header.php';
             </button>
 
         </nav>
+
         <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-6">
-                <h1 class="h3 mb-4 text-gray-800">Guru | Daftar Paket</h1>
+                <h1 class="h3 mb-4 text-gray-800">Toyota | Daftar Mobil</h1>
             </div>
             <div class="col-lg-6 text-right">
                 <a class="btn btn-primary" href="tambah.php">
@@ -27,80 +27,86 @@ require 'header.php';
                 </a>
             </div>
         </div>
-        <!-- Center-align the table -->
-        <div class="table-responsive mx-auto">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Gambar</th>
-                        <th>Nama Mobil</th>
-                        <th>Tahun</th>
-                        <th>Harga</th>
-                        <th>Tipe</th>
-                        <th>Deskripsi</th>
-                        <th class="text-center">Opsi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $no = 1;
-                    $sqld = "SELECT * FROM mobil";
-                    $resultd = mysqli_query($conn, $sqld);
 
-                    if ($resultd) {
-                        while ($rowd = mysqli_fetch_assoc($resultd)) {
-                            $id_mobil = $rowd['id_mobil'];
-                            $gambar = $rowd['gambar'];
-                            $nama = $rowd['nama'];
-                            $tahun = $rowd['tahun'];
-                            $harga = $rowd['harga'];
-                            $tipe = $rowd['tipe'];
-                            $deskripsi = $rowd['deskripsi'];
-                    ?>
+        <!-- Center-align the table within a card -->
+        <div class="card mx-auto">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
                             <tr>
-                                <td>
-                                    <?php echo $no++; ?>
-                                </td>
-                                <td style="text-align: center;">
-                                    <img src="uploads/cars/<?php echo $gambar; ?>" alt="<?php echo $gambar; ?>" style="max-height: 100px; max-width: 150px;" class="mx-auto d-block">
-                                </td>
-                                <td>
-                                    <?php echo $nama; ?>
-                                </td>
-                                <td>
-                                    <?php echo $tahun; ?>
-                                </td>
-                                <td>
-                                    <?php echo 'Rp ' . number_format($harga, 0, ',', '.'); ?>
-                                </td>
-                                <td>
-                                    <?php echo $tipe; ?>
-                                </td>
-                                <td>
-                                    <?php echo $deskripsi; ?>
-                                </td>
-                                <td class="text-center">
-                                    <a class="btn btn-warning" href="edit.php?id_paket=<?php echo $id_mobil ?>">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    <a class="btn btn-danger" href="#" onclick="confirmDelete(<?php echo $id_mobil; ?>)">
-                                        <i class="fas fa-trash"></i>
-                                    </a>
-                                </td>
-                        <?php
-                        }
-                    } else {
-                        echo "<tr><td colspan ='8'>Tidak ada data!</td></tr>";
-                    }
-                        ?>
+                                <th>No</th>
+                                <th>Gambar</th>
+                                <th>Nama Mobil</th>
+                                <th>Tahun</th>
+                                <th>Harga</th>
+                                <th>Tipe</th>
+                                <th>Deskripsi</th>
+                                <th class="text-center">Opsi</th>
                             </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <!-- Scroll to Top Button-->
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no = 1;
+                            $sqld = "SELECT * FROM mobil";
+                            $resultd = mysqli_query($conn, $sqld);
 
-    <?php
-    require 'footer.php';
-    ?>
+                            if ($resultd) {
+                                while ($rowd = mysqli_fetch_assoc($resultd)) {
+                                    $id_mobil = $rowd['id_mobil'];
+                                    $gambar = $rowd['gambar'];
+                                    $nama = $rowd['nama'];
+                                    $tahun = $rowd['tahun'];
+                                    $harga = $rowd['harga'];
+                                    $tipe = $rowd['tipe'];
+                                    $deskripsi = $rowd['deskripsi'];
+                            ?>
+                                    <tr>
+                                        <td>
+                                            <?php echo $no++; ?>
+                                        </td>
+                                        <td style="text-align: center;">
+                                            <img src="uploads/cars/<?php echo $gambar; ?>" alt="<?php echo $gambar; ?>" style="max-height: 100px; max-width: 150px;" class="mx-auto d-block">
+                                        </td>
+                                        <td>
+                                            <?php echo $nama; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $tahun; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo 'Rp ' . number_format($harga, 0, ',', '.'); ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $tipe; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $deskripsi; ?>
+                                        </td>
+                                        <td class="text-center">
+                                            <a class="btn btn-warning" href="edit.php?id_paket=<?php echo $id_mobil ?>">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                            <a class="btn btn-danger" href="#" onclick="confirmDelete(<?php echo $id_mobil; ?>)">
+                                                <i class="fas fa-trash"></i>
+                                            </a>
+                                        </td>
+                                <?php
+                                }
+                            } else {
+                                echo "<tr><td colspan ='8'>Tidak ada data!</td></tr>";
+                            }
+                                ?>
+                                    </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- End of Card -->
+
+    </div>
+    <!-- End of Main Content -->
+
+    <!-- Scroll to Top Button-->
+    <?php require 'footer.php'; ?>
--- dashboard/tambah.php
+++ dashboard/tambah.php
@@ -1,3 +1,106 @@
 <?php require 'header.php'; ?>
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nama = $_POST['nama'];
+    $tahun = $_POST['tahun'];
+    $harga = $_POST['harga'];
+    $tipe = $_POST['tipe'];
+    $deskripsi = $_POST['deskripsi'];
+
+    $gambar = $_FILES['gambar']['name'];
+    $gambar_temp = $_FILES['gambar']['tmp_name'];
+    $gambar_path = 'uploads/cars/' . $gambar;
+
+    move_uploaded_file($gambar_temp, $gambar_path);
+
+    $sql = "INSERT INTO mobil (nama, tahun, harga, tipe, deskripsi, gambar) VALUES ('$nama', '$tahun', '$harga', '$tipe', '$deskripsi', '$gambar')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'Mobil berhasil ditambahkan.',
+            }).then((result) => {
+                window.location.href = 'tambah.php';
+            });
+        </script>";
+    } else {
+        echo "<script>
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Error: " . mysqli_error($conn) . "',
+            });
+        </script>";
+    }
+    mysqli_close($conn);
+}
+?>
+
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
+
+        </nav>
+        <!-- Page Heading -->
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+
+                <!-- Card for adding a car -->
+                <div class="card">
+                    <div class="card-header">
+                        <h1 class="h3 mb-4 text-gray-800">Toyota | Tambah Mobil</h1>
+                    </div>
+                    <div class="card-body">
+
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="nama">Nama Mobil:</label>
+                                <input type="text" class="form-control" id="nama" name="nama" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="tahun">Tahun:</label>
+                                <input type="text" class="form-control" id="tahun" name="tahun" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="harga">Harga:</label>
+                                <input type="text" class="form-control" id="harga" name="harga" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="tipe">Tipe:</label>
+                                <input type="text" class="form-control" id="tipe" name="tipe" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="deskripsi">Deskripsi:</label>
+                                <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3" required></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="gambar">Gambar:</label>
+                                <input type="file" class="form-control-file" id="gambar" name="gambar" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Tambah Mobil</button>
+                        </form>
+                        <!-- End of Form -->
+                    </div>
+                </div>
+                <!-- End of Card -->
+
+            </div>
+        </div>
+
+    </div>
+</div>
 
 <?php require 'footer.php'; ?>
